8eeb47a4e860d6878a47ea6c0a49a96f0e2660d3
Alessandro Pilotti <ap@pilotti.it>
2013-10-31 22:14:51 +0200

    Move tools setup to FirstLogon

--- FirstLogon.ps1
+++ FirstLogon.ps1
@@ -1,1 +1,23 @@
+$ErrorActionPreference = "Stop"
+
+$virtPlatform = (gwmi Win32_ComputerSystem).Model
+Write-Host "Virtual platform: $virtPlatform"
+
+# TODO: Add XenServer / XCP
+switch($virtPlatform)
+{
+    "VMware Virtual Platform"
+    {
+        $Host.UI.RawUI.WindowTitle = "Installing VMware tools..."
+        E:\setup64.exe `/s `/v `"/qn REBOOT=ReallySuppress`" `/l $temp\vmware_tools_install.log
+        if (!$?) { throw "VMware tools setup failed"}
+    }
+    "KVM"
+    {
+        $Host.UI.RawUI.WindowTitle = "Installing VirtIO drivers..."
+        & cscript $temp\$virtioscript "E:\Win8\AMD64\*.inf"
+        if (!$?) { throw "InstallVirtIO failed"}
+        del $temp\$virtioscript
+    }
+}
 
--- Specialize.ps1
+++ Specialize.ps1
@@ -16,27 +16,6 @@ foreach($resource in $resources)
     (new-object System.Net.WebClient).DownloadFile($url, "$temp\$resource")
 }
 
-$virtPlatform = (gwmi Win32_ComputerSystem).Model
-Write-Host "Virtual platform: $virtPlatform"
-
-# TODO: Add XenServer / XCP
-switch($virtPlatform)
-{
-    "VMware Virtual Platform"
-    {
-        $Host.UI.RawUI.WindowTitle = "Installing VMware tools..."
-        E:\setup64.exe `/s `/v `"/qn REBOOT=ReallySuppress`" `/l $temp\vmware_tools_install.log
-        if (!$?) { throw "VMware tools setup failed"}
-    }
-    "KVM"
-    {
-        $Host.UI.RawUI.WindowTitle = "Installing VirtIO drivers..."
-        & cscript $temp\$virtioscript "E:\Win8\AMD64\*.inf"
-        if (!$?) { throw "InstallVirtIO failed"}
-        del $temp\$virtioscript
-    }
-}
-
 $Host.UI.RawUI.WindowTitle = "Configuring GPOs..."
 
 # Put the wallpaper in place
