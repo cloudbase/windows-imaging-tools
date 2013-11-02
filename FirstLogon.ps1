a9d84f1efe84f541eb1d5b5c7320343d1425e14f
Alessandro Pilotti <ap@pilotti.it>
2013-11-02 15:13:05 +0200

    Adds a cleaner exception handling

--- FirstLogon.ps1
+++ FirstLogon.ps1
@@ -5,39 +5,41 @@ $logonScriptPath = "$ENV:SystemRoot\Temp\Logon.ps1"
 $baseUrl = "https://raw.github.com/cloudbase/windows-openstack-imaging-tools/master"
 (new-object System.Net.WebClient).DownloadFile("$baseUrl/Logon.ps1", $logonScriptPath)
 
-function removeLogonScriptAndThrow($msg)
+try
 {
-    # Prevents the setup from proceeding
-    del $logonScriptPath
-    throw $msg
-}
+    $virtPlatform = (gwmi Win32_ComputerSystem).Model
+    Write-Host "Virtual platform: $virtPlatform"
 
-$virtPlatform = (gwmi Win32_ComputerSystem).Model
-Write-Host "Virtual platform: $virtPlatform"
-
-# TODO: Add XenServer / XCP
-switch($virtPlatform)
-{
-    "VMware Virtual Platform"
+    # TODO: Add XenServer / XCP
+    switch($virtPlatform)
     {
-        # Note: this command will generate a reboot.
-        # "/qn REBOOT=ReallySuppress" does not seem to work properly
-        $Host.UI.RawUI.WindowTitle = "Installing VMware tools..."
-        E:\setup64.exe `/s `/v `/qn `/l `"$ENV:Temp\vmware_tools_install.log`"
-        if (!$?) { removeLogonScriptAndThrow "VMware tools setup failed" }
+        "VMware Virtual Platform"
+        {
+            # Note: this command will generate a reboot.
+            # "/qn REBOOT=ReallySuppress" does not seem to work properly
+            $Host.UI.RawUI.WindowTitle = "Installing VMware tools..."
+            E:\setup64.exe `/s `/v `/qn `/l `"$ENV:Temp\vmware_tools_install.log`"
+            if (!$?) { throw "VMware tools setup failed" }
+        }
+        "KVM"
+        {
+            $Host.UI.RawUI.WindowTitle = "Downloading VirtIO drivers script..."
+            $virtioScriptPath = "$ENV:Temp\InstallVirtIODrivers.js"
+            $url = "https://raw.github.com/cloudbase/windows-openstack-imaging-tools/master/InstallVirtIODrivers.js"
+            (new-object System.Net.WebClient).DownloadFile($url, $virtioScriptPath)
+
+            $Host.UI.RawUI.WindowTitle = "Installing VirtIO drivers..."
+            & cscript $virtioScriptPath "E:\Win8\AMD64\*.inf"
+            if (!$?) { throw "InstallVirtIO failed" }
+            del $virtioScriptPath
+
+            shutdown /r /t 0
+        }
     }
-    "KVM"
+    catch
     {
-        $Host.UI.RawUI.WindowTitle = "Downloading VirtIO drivers script..."
-        $virtioScriptPath = "$ENV:Temp\InstallVirtIODrivers.js"
-        $url = "https://raw.github.com/cloudbase/windows-openstack-imaging-tools/master/InstallVirtIODrivers.js"
-        (new-object System.Net.WebClient).DownloadFile($url, $virtioScriptPath)
-
-        $Host.UI.RawUI.WindowTitle = "Installing VirtIO drivers..."
-        & cscript $virtioScriptPath "E:\Win8\AMD64\*.inf"
-        if (!$?) { removeLogonScriptAndThrow "InstallVirtIO failed"}
-        del $virtioScriptPath
-
-        shutdown /r /t 0
+        # Prevents the setup from proceeding
+        del $logonScriptPath
+        throw
     }
 }
