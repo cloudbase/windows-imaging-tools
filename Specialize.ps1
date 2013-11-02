6c871763b72e348cc52b43df384b789b0312957f
Alessandro Pilotti <ap@pilotti.it>
2013-11-02 04:11:55 +0200

    Avoid running mire scripts if the specialize script fails

--- Specialize.ps1
+++ Specialize.ps1
@@ -1,17 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-$gpoZipFile = "GPO.zip"
-$resources = @("FirstLogon.ps1", "Logon.ps1", $gpoZipFile)
-
 $temp = "$ENV:SystemRoot\Temp"
 $baseUrl = "https://raw.github.com/cloudbase/windows-openstack-imaging-tools/master"
-
-foreach($resource in $resources)
-{
-    $url = "$baseUrl/$resource"
-    $Host.UI.RawUI.WindowTitle = "Downloading $resource..."
-    (new-object System.Net.WebClient).DownloadFile($url, "$temp\$resource")
-}
 
 # Put the wallpaper in place
 $wallpaper_dir = "$ENV:SystemRoot\web\Wallpaper\Cloudbase"
@@ -26,7 +16,10 @@ $wallpaper = "Wallpaper-Cloudbase-2013.png"
 
 $Host.UI.RawUI.WindowTitle = "Configuring GPOs..."
 
+$gpoZipFile = "GPO.zip"
 $gpoZipPath = "$temp\$gpoZipFile"
+(new-object System.Net.WebClient).DownloadFile("$baseUrl/$gpoZipFile", $gpoZipPath)
+
 foreach($item in (New-Object -com shell.application).NameSpace($gpoZipPath).Items())
 {
     $yesToAll = 16
@@ -38,3 +31,10 @@ del $gpoZipPath
 # TODO: replace with with a netsh advfirewall command
 # possibly avoiding duplicates with "File and printer sharing (Echo Request - ICMPv[4,6]-In)"
 netsh firewall set icmpsetting 8
+
+foreach($resource in @("FirstLogon.ps1", "Logon.ps1"))
+{
+    $url = "$baseUrl/$resource"
+    $Host.UI.RawUI.WindowTitle = "Downloading $resource..."
+    (new-object System.Net.WebClient).DownloadFile($url, "$temp\$resource")
+}
