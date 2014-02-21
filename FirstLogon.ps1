96c2e32014c9598c9afcafb8614c25c6b10f404d
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-02-21 13:52:13 +0200

    OSVersion function refactoring in FirstLogon.ps1

--- FirstLogon.ps1
+++ FirstLogon.ps1
@@ -1,35 +1,36 @@
 $ErrorActionPreference = "Stop"
 
-function getOsVersionMinor(){
-    $osVersionMajor = [int]::Parse((Get-WmiObject Win32_OperatingSystem).Version[0])
-    if ($osVersionMajor -ne 6) {
-         throw "Unsupported Windows version" 
-    }
-    $osVersionMinor = [int]::Parse((Get-WmiObject Win32_OperatingSystem).Version[2])
-    return $osVersionMinor
+function getOSVersion(){
+    $v = (Get-WmiObject Win32_OperatingSystem).Version.Split('.')
+    
+    return New-Object psobject -Property @{
+        Major = [int]::Parse($v[0])
+        Minor = [int]::Parse($v[1])
+        Build = [int]::Parse($v[2])
+    }    
 }
 
 function getVirtioDriversFolder(){
     $architectureMapping = @{}
     $architectureMapping['32-bit']='X86'
-    $architectureMapping['64-bit']='AMD64'  
+    $architectureMapping['64-bit']='AMD64'
     $osVersionMapping = @{}
     $osVersionMapping[0]='VISTA'
     $osVersionMapping[1]='WIN7'
     $osVersionMapping[2]='WIN8'
-    $osVersionMapping[3]='WIN8'  
+    $osVersionMapping[3]='WIN8'
 
     $osArchitecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
     $archFolder = $architectureMapping[$osArchitecture]
-    
-    $osVersion = getOsVersionMinor
-    $versionFolder = $osVersionMapping[$osVersion]
-    if (! $versionFolder) { throw "Unsupported Windows version" }
-    
+
+    $osVersion = getOSVersion
+    $versionFolder = $osVersionMapping[$osVersion.Minor]
+    if (($osVersion.Major -ne 6) -or !$versionFolder) { throw "Unsupported Windows version" }
+
     $virtIOPath = Join-Path -Path $versionFolder -ChildPath $archFolder
     $drive = (gwmi Win32_CDROMDrive | where {(Test-Path (join-path -Path $_.Drive -ChildPath $virtIOPath ))}).Drive
-    if (! $drive) { throw "VirtIO drivers not found" }    
-    
+    if (! $drive) { throw "VirtIO drivers not found" }
+
     return join-path -Path $drive -ChildPath $virtIOPath | join-path -ChildPath "*.inf"
 }
 
@@ -42,7 +43,7 @@ try
     (new-object System.Net.WebClient).DownloadFile("$baseUrl/Logon.ps1", $logonScriptPath)
 
     $virtPlatform = (gwmi Win32_ComputerSystem).Model
-    Write-Host "Virtual platform: $virtPlatform"    
+    Write-Host "Virtual platform: $virtPlatform"
     # TODO: Add XenServer / XCP
     switch($virtPlatform)
     {
@@ -57,14 +58,14 @@ try
         {($_ -eq "KVM") -or ($_ -eq "Bochs")}
         {
             $virtioDriversPath = getVirtioDriversFolder
-            $osVersion = getOsVersionMinor
-            
-            if ($osVersion -gt 1) {
+            $osVersion = getOSVersion
+
+            if ($osVersion.Minor -gt 1) {
                 $Host.UI.RawUI.WindowTitle = "Downloading VirtIO drivers script..."
                 $virtioScriptPath = "$ENV:Temp\InstallVirtIODrivers.js"
                 $url = "$baseUrl/InstallVirtIODrivers.js"
-                (new-object System.Net.WebClient).DownloadFile($url, $virtioScriptPath)                
-                
+                (new-object System.Net.WebClient).DownloadFile($url, $virtioScriptPath)
+
                 Write-Host "Installing VirtIO drivers from: $virtioDriversPath"
                 & cscript $virtioScriptPath $virtioDriversPath
                 if (!$?) { throw "InstallVirtIO failed" }
@@ -86,11 +87,11 @@ try
                 Write-Host "Installing VirtIO drivers from: $virtioDriversPath"
                 Start-process -Wait pnputil "-i -a $virtioDriversPath"
                 if (!$?) { throw "InstallVirtIO failed" }
- 
+
                 del $virtioCertPath
                 $castore.Remove($cacert)
             }
-            
+
             shutdown /r /t 0
         }
     }
