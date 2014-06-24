c5f3a2273825284a80859599100f5e4f7a3446dc
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-06-25 01:33:49 +0300

    Adds support for the x86 Cloudbase-Init MSI

--- Logon.ps1
+++ Logon.ps1
@@ -41,20 +41,30 @@ try
     {
         $Host.UI.RawUI.WindowTitle = "Downloading Cloudbase-Init..."
 
-        $CloudbaseInitMsi = "$ENV:Temp\CloudbaseInitSetup_Beta.msi"
-        $CloudbaseInitMsiUrl = "http://www.cloudbase.it/downloads/CloudbaseInitSetup_Beta.msi"
+        $osArch = (Get-WmiObject  Win32_OperatingSystem).OSArchitecture
+        if($osArch -eq "64-bit")
+        {
+            $CloudbaseInitMsi = "CloudbaseInitSetup_Beta_x64.msi"
+        }
+        else
+        {
+            $CloudbaseInitMsi = "CloudbaseInitSetup_Beta_x86.msi"
+        }
+
+        $CloudbaseInitMsiPath = "$ENV:Temp\$CloudbaseInitMsi"
+        $CloudbaseInitMsiUrl = "http://www.cloudbase.it/downloads/$CloudbaseInitMsi"
         $CloudbaseInitMsiLog = "$ENV:Temp\CloudbaseInitSetup_Beta.log"
 
-        (new-object System.Net.WebClient).DownloadFile($CloudbaseInitMsiUrl, $CloudbaseInitMsi)
+        (new-object System.Net.WebClient).DownloadFile($CloudbaseInitMsiUrl, $CloudbaseInitMsiPath)
 
         $Host.UI.RawUI.WindowTitle = "Installing Cloudbase-Init..."
 
         $serialPortName = @(Get-WmiObject Win32_SerialPort)[0].DeviceId
 
-        $p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i $CloudbaseInitMsi /qn /l*v $CloudbaseInitMsiLog LOGGINGSERIALPORTNAME=$serialPortName"
+        $p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i $CloudbaseInitMsiPath /qn /l*v $CloudbaseInitMsiLog LOGGINGSERIALPORTNAME=$serialPortName"
         if ($p.ExitCode -ne 0)
         {
-            throw "Installing $CloudbaseInitMsi failed. Log: $CloudbaseInitMsiLog"
+            throw "Installing $CloudbaseInitMsiPath failed. Log: $CloudbaseInitMsiLog"
         }
 
          # We're done, remove LogonScript and disable AutoLogon
