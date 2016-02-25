6f0565a6c42146b639070e1e5138a89cb982c363
Costin GALAN <cgalan@cloudbasesolutions.com>
2016-02-25 05:27:05 +0200

    Implemented clean-up for update files and resources
    Signed-off-by: Costin GALAN <cgalan@cloudbasesolutions.com>

--- UnattendResources/Logon.ps1
+++ UnattendResources/Logon.ps1
@@ -2,10 +2,41 @@ $ErrorActionPreference = "Stop"
 $resourcesDir = "$ENV:SystemDrive\UnattendResources"
 $configIniPath = "$resourcesDir\config.ini"
 
+function Clean-UpdateResources {
+    $HOST.UI.RawUI.WindowTitle = "Running update resources cleanup"
+    # We're done, disable AutoLogon
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
+
+    # Cleanup
+    Remove-Item -Recurse -Force $resourcesDir
+    Remove-Item -Force "$ENV:SystemDrive\Unattend.xml"
+}
+
+function Clean-WindowsUpdates {
+    $HOST.UI.RawUI.WindowTitle = "Running Dism cleanup..."
+    if (([System.Environment]::OSVersion.Version.Major -gt 6) -or ([System.Environment]::OSVersion.Version.Minor -ge 2))
+    {
+        Dism.exe /Online /Cleanup-Image /StartComponentCleanup
+        if ($LASTEXITCODE)
+        {
+            throw "Dism.exe cleanup failed"
+        }
+    }
+}
+
+function Run-Defragment {
+    $HOST.UI.RawUI.WindowTitle = "Running Defrag..."
+    #Defragmenting all drives at normal priority
+    defrag.exe /C /H /V
+    if ($LASTEXITCODE)
+    {
+        throw "Defrag.exe failed"
+    }
+}
+
 try
 {
-    $needsReboot = $false
-
     Import-Module "$resourcesDir\ini.psm1"
     $installUpdates = Get-IniFileValue -Path $configIniPath -Section "DEFAULT" -Key "InstallUpdates" -Default $false -AsBoolean
 
@@ -34,43 +65,39 @@ try
         Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
         if (Get-WURebootStatus -Silent)
         {
-            $needsReboot = $true
             $Host.UI.RawUI.WindowTitle = "Updates installation finished. Rebooting."
             shutdown /r /t 0
+            exit 0
         }
     }
 
-    if(!$needsReboot)
+    Clean-WindowsUpdates
+
+    $Host.UI.RawUI.WindowTitle = "Installing Cloudbase-Init..."
+    $programFilesDir = $ENV:ProgramFiles
+
+    $CloudbaseInitMsiPath = "$resourcesDir\CloudbaseInit.msi"
+    $CloudbaseInitMsiLog = "$resourcesDir\CloudbaseInit.log"
+
+    $serialPortName = @(Get-WmiObject Win32_SerialPort)[0].DeviceId
+
+    $p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i $CloudbaseInitMsiPath /qn /l*v $CloudbaseInitMsiLog LOGGINGSERIALPORTNAME=$serialPortName"
+    if ($p.ExitCode -ne 0)
     {
-        $Host.UI.RawUI.WindowTitle = "Installing Cloudbase-Init..."
-        $programFilesDir = $ENV:ProgramFiles
-
-        $CloudbaseInitMsiPath = "$resourcesDir\CloudbaseInit.msi"
-        $CloudbaseInitMsiLog = "$resourcesDir\CloudbaseInit.log"
-
-        $serialPortName = @(Get-WmiObject Win32_SerialPort)[0].DeviceId
-
-        $p = Start-Process -Wait -PassThru -FilePath msiexec -ArgumentList "/i $CloudbaseInitMsiPath /qn /l*v $CloudbaseInitMsiLog LOGGINGSERIALPORTNAME=$serialPortName"
-        if ($p.ExitCode -ne 0)
-        {
-            throw "Installing $CloudbaseInitMsiPath failed. Log: $CloudbaseInitMsiLog"
-        }
-
-         # We're done, disable AutoLogon
-        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
-        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
-
-        # Cleanup
-        Remove-Item -Recurse -Force $resourcesDir
-        Remove-Item -Force "$ENV:SystemDrive\Unattend.xml"
-
-        $Host.UI.RawUI.WindowTitle = "Running SetSetupComplete..."
-        & "$programFilesDir\Cloudbase Solutions\Cloudbase-Init\bin\SetSetupComplete.cmd"
-
-        $Host.UI.RawUI.WindowTitle = "Running Sysprep..."
-        $unattendedXmlPath = "$programFilesDir\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
-        & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/shutdown `/unattend:"$unattendedXmlPath"
+        throw "Installing $CloudbaseInitMsiPath failed. Log: $CloudbaseInitMsiLog"
     }
+
+    $Host.UI.RawUI.WindowTitle = "Running SetSetupComplete..."
+    & "$programFilesDir\Cloudbase Solutions\Cloudbase-Init\bin\SetSetupComplete.cmd"
+
+    Run-Defragment
+
+    Clean-UpdateResources
+    
+    $Host.UI.RawUI.WindowTitle = "Running Sysprep..."
+    $unattendedXmlPath = "$programFilesDir\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
+    & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/shutdown `/unattend:"$unattendedXmlPath"
+
 }
 catch
 {
