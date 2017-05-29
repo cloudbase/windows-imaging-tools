0dcc3677613a9055032a887293be8d4531efb2a3
Andrei Tira <atira@cloudbasesolutions.com>
2017-05-29 06:07:15 -0700

    Updated Examples

--- Examples/create-windows-cloud-image.ps1
+++ Examples/create-windows-cloud-image.ps1
@@ -16,6 +16,8 @@ $ErrorActionPreference = "Stop"
 
 git submodule update --init
 Import-Module ..\WinImageBuilder.psm1
+Import-Module ..\Config.psm1
+Import-Module ..\UnattendResources\ini.psm1
 
 # The Windows image file path that will be generated
 $virtualDiskPath = "C:\images\my-windows-image.raw"
@@ -24,11 +26,12 @@ $virtualDiskPath = "C:\images\my-windows-image.raw"
 $wimFilePath = "D:\Sources\install.wim"
 
 # VirtIO ISO contains all the synthetic drivers for the KVM hypervisor
-$VirtIOISOPath = "C:\images\virtio.iso"
-$virtIODownloadLink = "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.126-2/virtio-win.iso"
+$virtIOISOPath = "C:\images\virtio.iso"
+# Note(avladu): Do not use stable 0.1.126 version because of this bug https://github.com/crobinso/virtio-win-pkg-scripts/issues/10
+$virtIODownloadLink = "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.133-2/virtio-win.iso"
 
 # Download the VirtIO drivers ISO from Fedora
-(New-Object System.Net.WebClient).DownloadFile($virtIODownloadLink, $VirtIOISOPath)
+(New-Object System.Net.WebClient).DownloadFile($virtIODownloadLink, $virtIOISOPath)
 
 # Extra drivers path contains the drivers for the baremetal nodes
 # Examples: Chelsio NIC Drivers, Mellanox NIC drivers, LSI SAS drivers, etc.
@@ -39,12 +42,23 @@ $extraDriversPath = "C:\drivers\"
 # Usually, the second image version is the Standard one
 $image = (Get-WimFileImagesInfo -WimFilePath $wimFilePath)[1]
 
+# The path were you want to create the config fille
+$configFilePath =".\config.ini"
+New-WindowsImageConfig -ConfigFilePath $configFilePath
+$fCfgPath = Resolve-Path $configFilePath
+
+#This is an example how to automate the image configuration file according to your needs
+Set-IniFileValue -Path $fCfgPath -Key "wim_file_path" -Value $wimFilePath
+Set-IniFileValue -Path $fCfgPath -Key "image_name" -Value $image.ImageName
+Set-IniFileValue -Path $fCfgPath -Key "image_path" -Value $virtualDiskPath
+Set-IniFileValue -Path $fCfgPath -Key "virtual_disk_format" -Value "RAW"
+Set-IniFileValue -Path $fCfgPath -Section "vm" -Key "disk_size" -Value (30GB)
+Set-IniFileValue -Path $fCfgPath -Section "drivers" -Key "virtio_iso_path" -Value $virtIOISOPath
+Set-IniFileValue -Path $fCfgPath -Section "drivers" -Key "drivers_path" -Value $extraDriversPath
+Set-IniFileValue -Path $fCfgPath -Section "updates" -Key "install_updates" -Value "True"
+Set-IniFileValue -Path $fCfgPath -Section "updates" -Key "purge_updates" -Value "True"
+Set-IniFileValue -Path $fCfgPath -Section "sysprep" -Key "disable_swap" -Value "True"
+
 # This scripts generates a raw image file that, after being started as an instance and
 # after it shuts down, it can be used with Ironic or KVM hypervisor in OpenStack.
-New-WindowsCloudImage -WimFilePath $wimFilePath -ImageName $image.ImageName `
-    -VirtualDiskPath $virtualDiskPath -SizeBytes 30GB -ProductKey $productKey `
-    -VirtualDiskFormat 'RAW' -DiskLayout 'BIOS' -VirtioISOPath $virtIOISOPath `
-    -ExtraDriversPath $extraDriversPath -InstallUpdates:$true `
-    -AdministratorPassword 'Pa$$w0rd' -InstallMAASHooks:$false `
-    -PurgeUpdates:$true -DisableSwap:$true
-
+New-WindowsCloudImage -ConfigFilePath $configFilePath
