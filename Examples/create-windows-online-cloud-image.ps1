be6296af0f132434191d0578ca9ed9ce0de1e65a
Andrei Tira <atira@cloudbasesolutions.com>
2017-05-29 06:07:15 -0700

    Updated Examples

--- Examples/create-windows-online-cloud-image.ps1
+++ Examples/create-windows-online-cloud-image.ps1
@@ -16,6 +16,8 @@ $ErrorActionPreference = "Stop"
 
 git submodule update --init
 Import-Module ..\WinImageBuilder.psm1
+Import-Module ..\Config.psm1
+Import-Module ..\UnattendResources\ini.psm1
 
 # The Windows image file path that will be generated
 $windowsImagePath = "C:\images\my-windows-image.raw.tgz"
@@ -24,12 +26,12 @@ $windowsImagePath = "C:\images\my-windows-image.raw.tgz"
 $wimFilePath = "D:\Sources\install.wim"
 
 # VirtIO ISO contains all the synthetic drivers for the KVM hypervisor
-$VirtIOISOPath = "C:\images\virtio.iso"
+$virtIOISOPath = "C:\images\virtio.iso"
 # Note(avladu): Do not use stable 0.1.126 version because of this bug https://github.com/crobinso/virtio-win-pkg-scripts/issues/10
 $virtIODownloadLink = "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.133-2/virtio-win.iso"
 
 # Download the VirtIO drivers ISO from Fedora
-(New-Object System.Net.WebClient).DownloadFile($virtIODownloadLink, $VirtIOISOPath)
+(New-Object System.Net.WebClient).DownloadFile($virtIODownloadLink, $virtIOISOPath)
 
 # Extra drivers path contains the drivers for the baremetal nodes
 # Examples: Chelsio NIC Drivers, Mellanox NIC drivers, LSI SAS drivers, etc.
@@ -44,12 +46,26 @@ $image = (Get-WimFileImagesInfo -WimFilePath $wimFilePath)[1]
 # are to be installed
 $switchName = 'external'
 
-# This scripts generates a raw tar.gz-ed image file, that can be used with MAAS
-New-WindowsOnlineImage -WimFilePath $wimFilePath -ImageName $image.ImageName `
-    -WindowsImagePath $windowsImagePath -Type 'MAAS' -ExtraFeatures @() `
-    -SizeBytes 30GB -CpuCores 4 -Memory 4GB -SwitchName $switchName `
-    -ProductKey $productKey -DiskLayout 'BIOS' -VirtioISOPath $virtIOISOPath `
-    -ExtraDriversPath $extraDriversPath `
-    -InstallUpdates:$true -AdministratorPassword 'Pa$$w0rd' `
-    -PurgeUpdates:$true -DisableSwap:$true
+# The path were you want to create the config fille
+$configFilePath =".\config.ini"
+New-WindowsImageConfig -ConfigFilePath $configFilePath
+$fCfgPath = Resolve-Path $configFilePath
 
+#This is an example how to automate the image configuration file according to your needs
+Set-IniFileValue -Path $fCfgPath -Key "wim_file_path" -Value $wimFilePath
+Set-IniFileValue -Path $fCfgPath -Key "image_name" -Value $image.ImageName
+Set-IniFileValue -Path $fCfgPath -Key "image_path" -Value $windowsImagePath
+Set-IniFileValue -Path $fCfgPath -Key "image_type" -Value "MAAS"
+Set-IniFileValue -Path $fCfgPath -Section "vm" -Key "cpu_count" -Value 4
+Set-IniFileValue -Path $fCfgPath -Section "vm" -Key "ram_size" -Value (4GB)
+Set-IniFileValue -Path $fCfgPath -Section "vm" -Key "disk_size" -Value (30GB)
+Set-IniFileValue -Path $fCfgPath -Key "install_maas_hooks" -Value "True"
+Set-IniFileValue -Path $fCfgPath -Section "vm" -Key "external_switch" -Value $switchName
+Set-IniFileValue -Path $fCfgPath -Section "drivers" -Key "virtio_iso_path" -Value $virtIOISOPath
+Set-IniFileValue -Path $fCfgPath -Section "drivers" -Key "drivers_path" -Value $extraDriversPath
+Set-IniFileValue -Path $fCfgPath -Section "updates" -Key "install_updates" -Value "True"
+Set-IniFileValue -Path $fCfgPath -Section "updates" -Key "purge_updates" -Value "True"
+Set-IniFileValue -Path $fCfgPath -Section "sysprep" -Key "disable_swap" -Value "True"
+
+# This scripts generates a raw tar.gz-ed image file, that can be used with MAAS
+New-WindowsOnlineImage -ConfigFilePath $configFilePath
