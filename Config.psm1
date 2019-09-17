08e535c79f74d3293e81127f2dee7fece935180e
Adrian Vladu <avladu@cloudbasesolutions.com>
2019-09-17 16:06:31 +0300

    Added option to disable_secure_boot for generation 2 VMs
    If set to true and the disk layout is UEFI, the secure boot firmware
    option will be disabled.
    This option is usefull for generationg UEFI images which have
    VirtIO drivers that are not supported by Secure Boot.

--- Config.psm1
+++ Config.psm1
@@ -188,8 +188,9 @@ function Get-AvailableConfigOptions {
                            See https://github.com/cloudbase/windows-openstack-imaging-tools/issues/192"},
         @{"Name" = "enable_active_mode"; "DefaultValue" = $false; "AsBoolean" = $true;
           "Description" = "If set to true, it will set the High Performance mode and some power mode
-                           and registry tweaks to prevent the machine from sleeping / hibernating."}
-
+                           and registry tweaks to prevent the machine from sleeping / hibernating."},
+        @{"Name" = "disable_secure_boot"; "GroupName" = "vm"; "DefaultValue" = $false; "AsBoolean" = $true;
+          "Description" = "If set to true and the disk layout is UEFI, the secure boot firmware option will be disabled."}
     )
 }
 
